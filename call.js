1b7cf32bc0224aed65cdb32a669231db48feae9d
joonha97 <kyn04138@naver.com>
2020-09-20 15:47:22 +0900

    홀로 리뷰함
    20개의 난수를 필드6로 보냈다.
    그래프는 https://thingspeak.com/channels/1136632/charts/6?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15
    에서 확인한다.

--- call.js
+++ call.js
@@ -2,13 +2,13 @@ var request = require("request")
 i=0
 function doit() {
 	v = Math.random() * 100
-	url = "https://api.thingspeak.com/update?api_key=O3X7P4RDFR3DX97Z&field2="+ v
+	url = "https://api.thingspeak.com/update?api_key=O3X7P4RDFR3DX97Z&field6="+ v
 	request(url, function(err, res, body) {
 		console.log(body)
 	})
-	
+
 	console.log(`i=${i}`)
-	if (i++ > 10) return
+	if (i++ > 20) return
 	setTimeout(doit, 20000)
 }
 console.log(`ready`)
